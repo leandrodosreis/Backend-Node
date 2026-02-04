62a454328041feafe347db1491f60a2642254925
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2026-02-04 15:26:40 -0300

    Calculo de media

--- Aula2/app.js
+++ Aula2/app.js
@@ -42,6 +42,22 @@ Versão: 1.0
  e -> and -> &&
  ou-> or -> ||
  nao -> not ->!
+
+ Formas de conversão de tipos de dados
+ parseInt() Permite converter conteudo em nmero inteiro
+ parseFloat() Permite converter um conteudo para flutuante
+ Number() Permite converter um conteudo em numero indiferente da sua origem
+ String() Permite converter um conteudo em string
+ Boolean() Permite converter um conrteudo em true ou false
+
+ typeof() vai indicar o tipo de dado de uma variavel(string, number, boolean ou object)
+
+ {} Json
+ [] array 
+ [[]] ou {[]} são matriz
+ porem ambos são objetos
+
+ toFixed() delimita as casas apos o ponto
 */ 
 
 //Import da biblioteca de entrada de dados
@@ -80,13 +96,29 @@ entradaDeDados.question('Digite o nome do aluno: ', function(nome){
 
                     //validação de numeros abaixo de 0 ou acima de 1000
                     }else if(nota1 < 0 || nota1 > 100 || nota2 < 0 || nota2 > 100 || nota3 < 0 || nota3 > 100 || nota4 < 0 || nota4 > 100 ){
-                        console.log('ERRO: Nota abixo de 0 ou acima de 100')
+                        console.log('ERRO: Somente nota abaixo de 0 ou acima de 100')
 
                     //validação de entradas somente de numeros
                     //isNaN() permite a validação de numeros ou letras
                     }else if(isNaN(nota1) || isNaN(nota2) || isNaN(nota3) || isNaN(nota4)){
                         console.log('ERRO: Somente numeros são aceitos')
+
+                    }else {
+                    
+                        //Calculando a media
+                        let statusAluno
+                        let media = (Number(nota1) + Number(nota2) + Number(nota3) + Number(nota4))/4
+
+                        if(media > 69.99 ){ statusAluno = "Aprovado"
+
+                        } else if(media < 50){ statusAluno = "Reprovado"
+    
+                        }else{ statusAluno = "Recuperação"}
+
+                         //toFixed é um metodo que permite fixar a quantidade de casas decimais
+                         console.log(`***************************\nAluno: ${nomeAluno} \nMedia final: ${media.toFixed(2)} \nStatus: ${statusAluno} \n***************************`) 
                     }
+
                 })
             })
         })
