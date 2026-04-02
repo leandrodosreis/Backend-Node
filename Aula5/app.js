6536f2c77c8d2440db3e3c58e12521bc1ed2ce5f
LEANDRO DOS REIS FILHO <25203700@CFP127ALUNO.LOCAL>
2026-04-02 16:12:47 -0300

    API pronta para ir ao postman

--- Aula5/app.js
+++ Aula5/app.js
@@ -38,76 +38,126 @@ app.use(cors(cosrsOptions))
 const estadosCidades = require('./modulo/funcoes.js')
 
 //endpoint para listar os estados (como detalhar a API na url /v1/senai/estados)
+//Retorna lista de estados
 app.get('/v1/senai/estados', function(request, response){
     let estados = estadosCidades.getListaDeEstados()
     if(estados){
-    response.json(estados)
-    response.status(200)} //requisição bem sucedida
-    else{
+        response.status(200) //requisição bem sucedida
+        response.json(estados)
+    }else{
+        response.status(404)
         response.json({'message': "Algo deu errado"})
-        response.status(400)
+        
     }
 })
 
-
-
+//Retorna dados de um estado filtrando pela sigla
 app.get('/v1/senai/dados/estado/:uf', function(request, response){
     let sigla = request.params.uf
     let estado = estadosCidades.getDadosEstado(sigla)
     if(estado){
-        response.json(estado)
         response.status(200)
+        response.json(estado)
     }else{
+        response.status(404)
         response.json({'message': "Algo deu errado"})
-        response.status(400)
+        
     }
 })
 
+//Retorna dados da capital filtrando pela sigla do estado
 app.get('/v1/senai/dados/capital/estado/:uf', function(request, response){
     let sigla = request.params.uf
     let capitalEstado = estadosCidades.getCapitalEstado(sigla)
     if(capitalEstado){
-        response.json(capitalEstado)
         response.status(200)
+        response.json(capitalEstado)
+        
     }else{
+        response.status(404)
         response.json({'message': "Algo deu errado"})
-        response.status(400)
+        
     }
 })
 
-app.get('/v1/senai/dados/estado/regiao/:uf', function(request, response){
-    let regiao = request.params.uf
+//Retorna os estados filtrando pela regiao
+app.get('/v1/senai/dados/estado/regiao/:regiao', function(request, response){
+    let regiao = request.params.regiao
     let estadoRegiao = estadosCidades.getEstadoRegiao(regiao)
     if(estadoRegiao){
-        response.json(estadoRegiao)
         response.status(200)
+        response.json(estadoRegiao)
+        
     }else{
+        response.status(404)
         response.json({'message': "Algo deu errado"})
-        response.status(400)
+        
     }
 })
 
-
-app.get('/v1/senai/dados/capitais/pais', function(request, response){
+//Retorna os estados que foram capitais do brasil
+app.get('/v1/senai/dados/estados/capitais/brasil', function(request, response){
     let capitalPais = estadosCidades.getCapitalPais()
     if(capitalPais){
-    response.json(capitalPais)
-    response.status(200)} //requisição bem sucedida
-    else{response.json({'message': "Algo deu errado"})
-    response.status(400)}
+        response.status(200) //requisição bem sucedida
+        response.json(capitalPais)
+    }else{
+        response.status(404)
+        response.json({'message': "Algo deu errado"})
+    }
 })
 
-
-app.get('/v1/senai/dados/cidades/:uf', function(request, response){
-    let sigla = request.params.uf
+//Retorna as cidades filtrando pela sigla dos estados
+app.get('/v1/senai/dados/cidades/estado/:cidade', function(request, response){
+    let sigla = request.params.cidade
     let cidade = estadosCidades.getCidades(sigla)
     if(cidade){
-        response.json(cidade)
         response.status(200)
+        response.json(cidade)
+        
     }else{
+        response.status(404)
         response.json({"message" : 'Algo deu erado'})
-        response.status(400)
+        
     }
+})
+
+app.get('/v1/senai/help', function(request, response){
+    let docAPI = {
+        "api-description" : "API para manipular dados de Estado e Cidades",
+        "date"            : "02/04/2026",
+        "development"     : "Leandro dos Reis Filho",
+        "version"         : 1.0,
+        "endpoints"       : [
+            {
+                "rota1"    : "v1/senai/estados",
+                "description"   :   "Retorna a lista de todos os estados"
+            },
+            {
+                "rota2"    : "v1/senai/dados/estados/sp",
+                "description"   :   "Retorna dados de um estado filtrando pela sigla"
+            },
+            {
+                "rota3"    : "v1/senai/dados/capital/estados/sp",
+                "description"   :   "Retorna dados da capital de um estado filtrando pela sigla"
+            },
+            {
+                "rota4"    : "v1/senai/dados/estados/regiao/sul",
+                "description"   :   "Retorna a lista doa estados filtrando pela região"
+            },
+            {
+                "rota5"    : "v1/senai/dados/estados/capitais/brasil",
+                "description"   :   "Retorna a lista de todos os estados que ja foram capitais do brasil"
+            },
+            {
+                "rota6"    : "v1/senai/dados/cidades/estados/sp",
+                "description"   :   "Retorna a lista de cidades filtrando pela sigla do estado"
+            }
+        ]
+    }
+
+    response.status(200)
+    response.json(docAPI)
 })
 
 
